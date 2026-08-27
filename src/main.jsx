175ad03dac2b117389f78d27724ac256b7451bd8
t1629 <walterburique@gmail.com>
2026-08-27 13:40:29 -0300

    pudiendo hacer que cualquiera va la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Harry-Potter">
     <StrictMode>
       <App />
     </StrictMode>
